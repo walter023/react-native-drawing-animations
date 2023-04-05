2d9744d004dc3a3ead6699b5a1e925a4137a2c17
Walter Munoz <walter.munoz@gmail.com>
2023-04-05 10:11:29 +1000

    Addding bezier curve interpolation

--- src/components/bezierCurve/BezierCurve.tsx
+++ src/components/bezierCurve/BezierCurve.tsx
@@ -1,12 +1,21 @@
 import React, { Fragment, useEffect } from 'react';
 import { StyleSheet, View, useWindowDimensions } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
-import Animated, { runOnJS, useAnimatedProps, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
+import Animated, {
+  runOnJS,
+  useAnimatedProps,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 
 import { Points as PointsPros } from '../../../types';
 import { ControlPointsInitState } from '../../models';
 import { ControlPoint } from './ControlPoint';
 import { Color, R } from '../../constants';
+import { lerp } from '../../helpers';
 
 export const Beziercurve: React.FC = () => {
   const duration = { duration: 2000 };
@@ -20,28 +29,46 @@ export const Beziercurve: React.FC = () => {
   const SideBX = useSharedValue<number>(ControlPointsInitState.p2.x);
   const SideBY = useSharedValue<number>(ControlPointsInitState.p2.y);
 
+  const bezierT = useSharedValue<number>(0);
+  const bezeirCurveX = useSharedValue<number>(SideBX.value);
+  const bezeirCurveY = useSharedValue<number>(SideBY.value);
+
   const setCtrlPointPosition = (point: PointsPros): void => {
     if (point) {
       const newState = { ...ctrlPoints.value, ...point };
       ctrlPoints.value = newState;
+      //initial positions
       SideAX.value = ctrlPoints.value.p1.x;
       SideAY.value = ctrlPoints.value.p1.y;
       SideBX.value = ctrlPoints.value.p2.x;
       SideBY.value = ctrlPoints.value.p2.y;
+      bezeirCurveX.value = SideBX.value;
+      bezeirCurveY.value = SideBY.value;
+      bezierT.value = 0;
       runOnJS(updateInterpolation)();
     }
   };
-
-  useEffect(() => {
-    updateInterpolation();
-  });
 
   const updateInterpolation = () => {
     SideAX.value = withRepeat(withTiming(ctrlPoints.value.p0.x, duration), -1, true);
     SideAY.value = withRepeat(withTiming(ctrlPoints.value.p0.y, duration), -1, true);
     SideBX.value = withRepeat(withTiming(ctrlPoints.value.p1.x, duration), -1, true);
     SideBY.value = withRepeat(withTiming(ctrlPoints.value.p1.y, duration), -1, true);
+    bezierT.value = withRepeat(withTiming(1, duration), -1, true);
   };
+
+  useEffect(() => {
+    updateInterpolation();
+  });
+
+  useDerivedValue(() => {
+    const x1 = SideBX.value;
+    const y1 = SideBY.value;
+    const x2 = SideAX.value;
+    const y2 = SideAY.value;
+    bezeirCurveX.value = lerp(x1, x2, bezierT.value);
+    bezeirCurveY.value = lerp(y1, y2, bezierT.value);
+  }, [bezierT, SideAX, SideAY, SideBX, SideBY]);
 
   const triangulePath = useAnimatedProps(() => {
     const starts = `${ctrlPoints.value.p0.x + R} ${ctrlPoints.value.p0.y + R}`;
@@ -77,6 +104,11 @@ export const Beziercurve: React.FC = () => {
     };
   });
 
+  const bezierBall = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: bezeirCurveX.value }, { translateY: bezeirCurveY.value }],
+    };
+  });
   return (
     <View style={styles.container}>
       <ControlPoint setCtrlPointPosition={setCtrlPointPosition} style={styles.p0} position={ctrlPoints.value.p0} id="p0" />
@@ -90,6 +122,7 @@ export const Beziercurve: React.FC = () => {
       <Fragment>
         <Animated.View style={[styles.l1, ballSideA]} />
         <Animated.View style={[styles.l2, ballSideB]} />
+        <Animated.View style={[styles.p, bezierBall]} />
       </Fragment>
     </View>
   );
@@ -113,6 +146,14 @@ const styles = StyleSheet.create({
     backgroundColor: Color.GREEN,
     left: -5,
     zIndex: 1,
+  },
+  p: {
+    width: 20,
+    height: 20,
+    borderRadius: R,
+    backgroundColor: Color.CYAN,
+    position: 'absolute',
+    zIndex: -1,
   },
   canvas: {
     zIndex: -1,
