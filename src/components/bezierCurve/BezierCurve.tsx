76b115032090794e989b0279fe7b71ad782f0567
Walter Munoz <walter.munoz@gmail.com>
2023-04-05 11:15:15 +1000

    Completing bezier curve logic

--- src/components/bezierCurve/BezierCurve.tsx
+++ src/components/bezierCurve/BezierCurve.tsx
@@ -18,57 +18,57 @@ import { Color, R } from '../../constants';
 import { lerp } from '../../helpers';
 
 export const Beziercurve: React.FC = () => {
-  const duration = { duration: 2000 };
+  const DURATION = { duration: 3000 };
   const AnimatedPath = Animated.createAnimatedComponent(Path);
   const { width, height } = useWindowDimensions();
   const ctrlPoints = useSharedValue<PointsPros>(ControlPointsInitState);
 
-  const SideAX = useSharedValue<number>(ControlPointsInitState.p1.x);
-  const SideAY = useSharedValue<number>(ControlPointsInitState.p1.y);
+  const sideAX = useSharedValue<number>(ControlPointsInitState.p1.x);
+  const sideAY = useSharedValue<number>(ControlPointsInitState.p1.y);
 
-  const SideBX = useSharedValue<number>(ControlPointsInitState.p2.x);
-  const SideBY = useSharedValue<number>(ControlPointsInitState.p2.y);
+  const sideBX = useSharedValue<number>(ControlPointsInitState.p2.x);
+  const sideBY = useSharedValue<number>(ControlPointsInitState.p2.y);
 
   const bezierT = useSharedValue<number>(0);
-  const bezeirCurveX = useSharedValue<number>(SideBX.value);
-  const bezeirCurveY = useSharedValue<number>(SideBY.value);
+  const bezeirCurveX = useSharedValue<number>(sideBX.value);
+  const bezeirCurveY = useSharedValue<number>(sideBY.value);
 
   const setCtrlPointPosition = (point: PointsPros): void => {
     if (point) {
       const newState = { ...ctrlPoints.value, ...point };
       ctrlPoints.value = newState;
       //initial positions
-      SideAX.value = ctrlPoints.value.p1.x;
-      SideAY.value = ctrlPoints.value.p1.y;
-      SideBX.value = ctrlPoints.value.p2.x;
-      SideBY.value = ctrlPoints.value.p2.y;
-      bezeirCurveX.value = SideBX.value;
-      bezeirCurveY.value = SideBY.value;
+      sideAX.value = ctrlPoints.value.p1.x;
+      sideAY.value = ctrlPoints.value.p1.y;
+      sideBX.value = ctrlPoints.value.p2.x;
+      sideBY.value = ctrlPoints.value.p2.y;
+      bezeirCurveX.value = sideBX.value;
+      bezeirCurveY.value = sideBY.value;
       bezierT.value = 0;
-      runOnJS(updateInterpolation)();
+      runOnJS(runInterpolation)();
     }
   };
 
-  const updateInterpolation = () => {
-    SideAX.value = withRepeat(withTiming(ctrlPoints.value.p0.x, duration), -1, true);
-    SideAY.value = withRepeat(withTiming(ctrlPoints.value.p0.y, duration), -1, true);
-    SideBX.value = withRepeat(withTiming(ctrlPoints.value.p1.x, duration), -1, true);
-    SideBY.value = withRepeat(withTiming(ctrlPoints.value.p1.y, duration), -1, true);
-    bezierT.value = withRepeat(withTiming(1, duration), -1, true);
+  const runInterpolation = () => {
+    sideAX.value = withRepeat(withTiming(ctrlPoints.value.p0.x, DURATION), -1, true);
+    sideAY.value = withRepeat(withTiming(ctrlPoints.value.p0.y, DURATION), -1, true);
+    sideBX.value = withRepeat(withTiming(ctrlPoints.value.p1.x, DURATION), -1, true);
+    sideBY.value = withRepeat(withTiming(ctrlPoints.value.p1.y, DURATION), -1, true);
+    bezierT.value = withRepeat(withTiming(1, DURATION), -1, true);
   };
 
   useEffect(() => {
-    updateInterpolation();
+    runInterpolation();
   });
 
   useDerivedValue(() => {
-    const x1 = SideBX.value;
-    const y1 = SideBY.value;
-    const x2 = SideAX.value;
-    const y2 = SideAY.value;
+    const x1 = sideBX.value;
+    const y1 = sideBY.value;
+    const x2 = sideAX.value;
+    const y2 = sideAY.value;
     bezeirCurveX.value = lerp(x1, x2, bezierT.value);
     bezeirCurveY.value = lerp(y1, y2, bezierT.value);
-  }, [bezierT, SideAX, SideAY, SideBX, SideBY]);
+  }, [bezierT, sideAX, sideAY, sideBX, sideBY]);
 
   const triangulePath = useAnimatedProps(() => {
     const starts = `${ctrlPoints.value.p0.x + R} ${ctrlPoints.value.p0.y + R}`;
@@ -80,27 +80,27 @@ export const Beziercurve: React.FC = () => {
   });
 
   const connectorPath = useAnimatedProps(() => {
-    const lineStarts = `${SideAX.value + R} ${SideAY.value + R}`;
-    const lineEnds = `${SideBX.value + R} ${SideBY.value + R}`;
+    const lineStarts = `${sideAX.value + R} ${sideAY.value + R}`;
+    const lineEnds = `${sideBX.value + R} ${sideBY.value + R}`;
     return { d: `M${lineStarts}L${lineEnds}` };
   });
 
   const bezierCurvePath = useAnimatedProps(() => {
-    const curveStarts = `${SideAX.value + R} ${SideAY.value + R}`;
-    const curvePositionAndDepth = `${SideBX.value + R} ${SideBY.value + R}`;
-    const curveEnds = `${ctrlPoints.value.p2.x + R}  ${ctrlPoints.value.p2.y + R}`;
+    const curveStarts = `${bezeirCurveX.value + R} ${bezeirCurveY.value + R}`;
+    const curvePositionAndDepth = `${sideAX.value + R} ${sideAY.value + R}`;
+    const curveEnds = `${ctrlPoints.value.p0.x + R}  ${ctrlPoints.value.p0.y + R}`;
     return { d: `M${curveStarts} Q${curvePositionAndDepth} ${curveEnds}` };
   });
 
   const ballSideA = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: SideAX.value }, { translateY: SideAY.value }],
+      transform: [{ translateX: sideAX.value }, { translateY: sideAY.value }],
     };
   });
 
   const ballSideB = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: SideBX.value }, { translateY: SideBY.value }],
+      transform: [{ translateX: sideBX.value }, { translateY: sideBY.value }],
     };
   });
 
@@ -153,7 +153,7 @@ const styles = StyleSheet.create({
     borderRadius: R,
     backgroundColor: Color.CYAN,
     position: 'absolute',
-    zIndex: -1,
+    zIndex: 1,
   },
   canvas: {
     zIndex: -1,
