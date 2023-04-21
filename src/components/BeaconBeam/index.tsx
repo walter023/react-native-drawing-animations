018ea9bc94580803d1ec4f49b5f112a05debfd68
Walter Munoz <walter.munoz@gmail.com>
2023-04-21 13:15:30 +1000

    Adding vector reflection logic

--- src/components/BeaconBeam/index.tsx
+++ src/components/BeaconBeam/index.tsx
@@ -17,19 +17,19 @@ import { Color } from '../../constants';
 
 const RADIUS = 20;
 const DURATION = { duration: 8000 };
-const ANGLE = { startAngle: 90, endAngle: 260 };
+const ANGLE = { startAngle: 100, endAngle: 250 };
 
 export const BeaconBeam: React.FC = () => {
   const { width, height } = useWindowDimensions();
   const { startAngle, endAngle } = ANGLE;
   const yTurret = height / 1.2;
   const xTurret = width / 2;
-  
+
   const isAnimationCancel = useSharedValue<boolean>(false);
-  const vectorX = useSharedValue<number>(0);
-  const vertorY = useSharedValue<number>(0);
+  const xRay = useSharedValue<number>(0);
+  const yRay = useSharedValue<number>(0);
   const angle = useSharedValue<number>(startAngle);
-  
+
   const AnimatedPath = Animated.createAnimatedComponent<PathProps>(Path);
 
   useEffect(() => {
@@ -57,9 +57,9 @@ export const BeaconBeam: React.FC = () => {
 
   const laser = useDerivedValue(() => {
     const radians = ((angle.value + startAngle) * Math.PI) / 180; // add 90 to adjust for the initial angle
-    vectorX.value = Math.round(xTurret + Math.cos(radians) * height);
-    vertorY.value = Math.round(yTurret + Math.sin(radians) * height);
-    return `M${xTurret},${yTurret} L${vectorX.value},${vertorY.value}`;
+    xRay.value = Math.round(xTurret + Math.cos(radians) * height);
+    yRay.value = Math.round(yTurret + Math.sin(radians) * height);
+    return `M${xTurret},${yTurret} L${xRay.value},${yRay.value}`;
   }, [angle.value]);
 
   const laserPath = useAnimatedProps(() => {
@@ -78,8 +78,8 @@ export const BeaconBeam: React.FC = () => {
    * y = slope * (xEdge - xTurret) + yTurret,
    * xEdge is the x-coordinate of the edge point. xEdge = (edge === 'right') ? width : 0;.
    */
-  const hitPoint = useDerivedValue(() => {
-    const laserVector = { x: vectorX.value - xTurret, y: vertorY.value - yTurret };
+  const intersectionPoint = useDerivedValue(() => {
+    const laserVector = { x: xRay.value - xTurret, y: yRay.value - yTurret };
     const slope = laserVector.y / laserVector.x;
     let hitPoint, xEdge, yEdge;
 
@@ -104,9 +104,9 @@ export const BeaconBeam: React.FC = () => {
     }
     // Check for edge case where laser is vertical
     else {
-      hitPoint = { x: vectorX.value, y: vertorY.value > height ? height : RADIUS }; //y: 0 };
+      hitPoint = { x: xRay.value, y: yRay.value > height ? height : RADIUS }; //y: 0 };
     }
-
+    // draw the hit point
     const path = `
     M${hitPoint.x},${hitPoint.y}
     m -${RADIUS}, 0
@@ -114,11 +114,29 @@ export const BeaconBeam: React.FC = () => {
     a ${RADIUS},${RADIUS} 0 1,0 ${-RADIUS * 2},0
     `;
 
-    return path;
-  }, [vectorX.value, vertorY.value]);
+    return { path, hitPoint };
+  }, [xRay.value, yRay.value]);
 
   const hitPointPath = useAnimatedProps(() => {
-    return { d: hitPoint.value };
+    return { d: intersectionPoint.value.path };
+  });
+
+  const rayPath = useAnimatedProps(() => {
+    const normalVector = { x: -1, y: 0 }; // surface is flat and horizontal
+    const xHitPoint = intersectionPoint.value.hitPoint.x;
+    const yHitPoint = intersectionPoint.value.hitPoint.y;
+    const incomingVector = {
+      x: xRay.value - xHitPoint,
+      y: yRay.value - yHitPoint,
+    };
+    const dotProduct = incomingVector.x * normalVector.x + incomingVector.y * normalVector.y;
+    const reflectedVector = {
+      x: incomingVector.x - 2 * dotProduct * normalVector.x,
+      y: incomingVector.y - 2 * dotProduct * normalVector.y,
+    };
+    const reflectedPath = `M${xHitPoint},${yHitPoint} L${xHitPoint + reflectedVector.x},${yHitPoint + reflectedVector.y}`;
+
+    return { d: reflectedPath };
   });
 
   return (
@@ -127,6 +145,7 @@ export const BeaconBeam: React.FC = () => {
       <Svg height={height} width={width}>
         <AnimatedPath animatedProps={laserPath} fill="none" strokeWidth={2} stroke={Color.RED} />
         <AnimatedPath animatedProps={hitPointPath} fill={Color.VIVID_YELLOW} strokeWidth={2} stroke={Color.GREEN} />
+        <AnimatedPath animatedProps={rayPath} fill={Color.STRONG_CYAN} strokeWidth={2} stroke={Color.GREEN} />
       </Svg>
 
       <Animated.View style={[styles.turret, animatedStyle]}>
